7cf7622c02d64e3d23aad32d338f8a28d41f6f17
Vojtech Dobes <dobes.vojtech@gmail.com>
2011-05-17 18:39:02 +0200

    added classes for payment buttons

---
+++ Gopay/PaymentButton.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Gopay Helper with Happy API
+ * 
+ * 
+ * @author Vojtech Dobes
+ */
+
+namespace VojtechDobes\Gopay;
+
+use Nette\Forms\SubmitButton;
+
+/**
+ * Payment button
+ *
+ * @property-read   $channel
+ */
+class PaymentButton extends SubmitButton
+{
+	
+	/** @var string */
+	private $channel;
+	
+	public function setChannel($channel)
+	{
+		$this->channel = $channel;
+	}
+	
+	public function getChannel()
+	{
+		return $this->channel;
+	}
+
+
+}

048be7ada7fde2d3d8dc25a44d25cb318c41c6c6
Vojtech Dobes <dobes.vojtech@gmail.com>
2012-04-11 08:58:47 +0200

    ReturnedPayment: richer access to result info

--- Gopay/ReturnedPayment.php
+++ Gopay/ReturnedPayment.php
@@ -64,8 +64,8 @@ class ReturnedPayment extends Payment
 	const FAILURE_SUPERCASH = -3,
 		FAILURE_BANK        = -7;
 
-	/** @var int */
-	private $failureInfo;
+	/** @var array */
+	private $result;
 
 
 	/**
@@ -75,7 +75,68 @@ class ReturnedPayment extends Payment
 	 */
 	public function isPaid()
 	{
-		$this->failureInfo = GopaySoap::isEshopPaymentDone(
+		$this->getStatus();
+		return $this->result['code'] === GopayHelper::PAYMENT_DONE;
+	}
+
+
+	/**
+	 * Returns TRUE if payment is waiting to be paid
+	 *
+	 * @return bool
+	 */
+	public function isWaiting()
+	{
+		$this->getStatus();
+		return $this->result['code'] === GopayHelper::WAITING;
+	}
+
+
+	/**
+	 * Returns TRUE if payment is canceled
+	 *
+	 * @return bool
+	 */
+	public function isCanceled()
+	{
+		$this->getStatus();
+		return $this->result['code'] === GopayHelper::CANCELED;
+	}
+
+
+	/**
+	 * Returns TRUE if payment time limit already expired
+	 *
+	 * @return bool
+	 */
+	public function isTimeouted()
+	{
+		$this->getStatus();
+		return $this->result['code'] === GopayHelper::CANCELED;
+	}
+
+
+	/**
+	 * Returns description of payment status received from Gopay WS
+	 *
+	 * @return string
+	 */
+	public function getDescription()
+	{
+		$this->getStatus();
+		return $this->result['description'];
+	}
+
+
+	/**
+	 * Receives status of payment from Gopay WS
+	 *
+	 * @return array
+	 */
+	public function getStatus()
+	{
+		if ($this->result !== NULL) return $this->result;
+		return $this->result = GopaySoap::isEshopPaymentDone(
 			$this->valuesToBeVerified['paymentSessionId'],
 			$this->gopayIdentification->id,
 			$this->variable,
@@ -83,8 +144,6 @@ class ReturnedPayment extends Payment
 			$this->product,
 			$this->gopayIdentification->secretKey
 		);
-
-		return $this->failureInfo === 1;
 	}
 
 }

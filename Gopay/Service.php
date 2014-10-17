contributte/gopay

commit 44182c46ab899eeaf20d89bc66983377fd04f01d

Added 'bindPaymentButton' returning the button

Gopay/Service.php
@@ -456,19 +456,41 @@ class Service extends Nette\Object
 	public function bindPaymentButtons(Nette\Forms\Container $form, $callbacks)
 	{
 		foreach ($this->allowedChannels as $name => $channel) {
-			if (!isset($channel->image)) {
-				$button = $form['gopayChannel' . $name] = new PaymentButton($name, $channel->title);
-			} else {
-				$button = $form['gopayChannel' . $name] = new ImagePaymentButton($name, $channel->image, $channel->title);
-			}
-
-			if (!is_array($callbacks)) $callbacks = array($callbacks);
-			foreach ($callbacks as $callback) {
-				$button->onClick[] = $callback;
-			}
-
-			$this->allowedChannels[$name]->control = 'gopayChannel' . $name;
+			$this->bindPaymentButton($channel, $form, $callbacks);
 		}
+	}
+
+
+
+	/**
+	 * Binds form to Gopay
+	 * - adds one payment button for given channel
+	 *
+	 * @param  string|stdClass
+	 * @param  Form
+	 * @param  array|callable
+	 */
+	public function bindPaymentButton($channel, Nette\Forms\Container $form, $callbacks = array())
+	{
+		if (!$channel instanceof \stdClass) {
+			if (!isset($this->allowedChannels[$channel])) {
+				throw new \InvalidArgumentException("Channel '$channel' is not allowed.");
+			}
+			$channel = $this->allowedChannels[$channel];
+		}
+
+		if (!isset($channel->image)) {
+			$button = $form['gopayChannel' . $name] = new PaymentButton($name, $channel->title);
+		} else {
+			$button = $form['gopayChannel' . $name] = new ImagePaymentButton($name, $channel->image, $channel->title);
+		}
+
+		if (!is_array($callbacks)) $callbacks = array($callbacks);
+		foreach ($callbacks as $callback) {
+			$button->onClick[] = $callback;
+		}
+
+		return $button;
 	}
 
 

commit b763f0da83c562296a6f56aab5ff56e8914acca8

Fixed BC break introduced in 44182c46ab899eeaf20d89bc66983377fd04f01d

Gopay/Service.php
@@ -481,6 +481,8 @@ class Service extends Nette\Object
 			$button = $form['gopayChannel' . $channel->name] = new ImagePaymentButton($channel->name, $channel->image, $channel->title);
 		}
 
+		$channel->control = 'gopayChannel' . $channel->name;
+
 		if (!is_array($callbacks)) $callbacks = array($callbacks);
 		foreach ($callbacks as $callback) {
 			$button->onClick[] = $callback;
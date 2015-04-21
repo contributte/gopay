54b998af4ed42a9ae68eb4d4b0b87d0790d863bb
Milan Felix Sulc <rkfelix@gmail.com>
2015-04-21 17:37:47 +0200

    Added GopayHolder - static singleton class that holds reference to GopaySoap and GopayHelper. It helps testing.

---
+++ Gopay/GopayHolder.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Markette - payment methods integration for Nette Framework
+ *
+ * @license New BSD
+ * @package Markette
+ * @author  Vojtìch Dobeš
+ */
+
+namespace Markette\Gopay;
+
+use Markette\Gopay\Api\GopayHelper;
+use Markette\Gopay\Api\GopaySoap;
+
+
+/**
+ * GopayHolder - singleton - hold helper and soap instances.
+ *
+ * @author Vojtìch Dobeš
+ * @author Jan Skrasek
+ */
+class GopayHolder
+{
+
+    /** @var GopayHolder */
+    private static $instance;
+
+    /** @var GopayHelper */
+    private static $helper;
+
+    /** @var GopaySoap */
+    private static $soap;
+
+    private function __construct()
+    {
+        // Disable constructor
+    }
+
+    /**
+     * @return GopayHolder
+     */
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+            self::$helper = new GopayHelper();
+            self::$soap = new GopaySoap();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * @return GopayHelper
+     */
+    public function getHelper() {
+        return self::$helper;
+    }
+
+    /**
+     * @param mixed $helper
+     */
+    public function setHelper($helper) {
+        self::$helper = $helper;
+    }
+
+    /**
+     * @return GopaySoap
+     */
+    public function getSoap()
+    {
+        return self::$soap;
+    }
+
+    /**
+     * @param mixed $soap
+     */
+    public function setSoap($soap)
+    {
+        self::$soap = $soap;
+    }
+
+}
